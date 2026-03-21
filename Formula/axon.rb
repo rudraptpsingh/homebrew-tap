cbb47da416a01461cad3d65a74cc623cef3b0c07
rudraptpsingh <rudra.ptp.singh@gmail.com>
2026-03-21 20:55:36 +0530

    Bump to v0.1.4: add disk pressure alerts

--- Formula/axon.rb
+++ Formula/axon.rb
@@ -1,12 +1,17 @@
 class Axon < Formula
   desc "Local hardware intelligence for AI coding agents"
   homepage "https://github.com/rudraptpsingh/axon"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
-  on_arm do
-    url "https://github.com/rudraptpsingh/axon/releases/download/v#{version}/axon-aarch64-apple-darwin"
-    sha256 "eef09d2c7d82c6ae6d63ac12830dc3f696e4bf96dab10f579314305c264429cc"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/rudraptpsingh/axon/releases/download/v#{version}/axon-aarch64-apple-darwin"
+      sha256 "1a774a7e4db24234594ac67878ed0bf0122b071655b00f35ad3d74599df05870"
+    else
+      url "https://github.com/rudraptpsingh/axon/releases/download/v#{version}/axon-x86_64-apple-darwin"
+      sha256 "c87f6282377e8dcdb237adf434c7da46d763140ad20c470740eff3b6f17afb41"
+    end
   end
 
   def install

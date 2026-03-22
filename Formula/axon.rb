8fb955b39975cae1f0d2247e68e0d23c39906cdf
rudraptpsingh <rudra.ptp.singh@gmail.com>
2026-03-22 14:05:32 +0530

    Update axon to 0.2.0 with real release SHA256 values

--- Formula/axon.rb
+++ Formula/axon.rb
@@ -7,20 +7,20 @@ class Axon < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/rudraptpsingh/axon/releases/download/v#{version}/axon-aarch64-apple-darwin"
-      sha256 "PLACEHOLDER_AARCH64_DARWIN"
+      sha256 "14d4adba6895fd9829f60b217cc67b8382bbda27a2eab7a3f3f858322be4bbe5"
     else
       url "https://github.com/rudraptpsingh/axon/releases/download/v#{version}/axon-x86_64-apple-darwin"
-      sha256 "PLACEHOLDER_X86_64_DARWIN"
+      sha256 "9ea27fa5fe7afb5c4329ada7afce17c8f98adf5e0d8adaf26e8f186c38aefb3d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/rudraptpsingh/axon/releases/download/v#{version}/axon-aarch64-unknown-linux-gnu"
-      sha256 "PLACEHOLDER_AARCH64_LINUX"
+      sha256 "7b35dc8d6b7bae160648519e769b5446d25662bbffe53b34418c4ff9a8bb56c4"
     else
       url "https://github.com/rudraptpsingh/axon/releases/download/v#{version}/axon-x86_64-unknown-linux-gnu"
-      sha256 "PLACEHOLDER_X86_64_LINUX"
+      sha256 "7d80026d147466e3639ebcde7ca7d5cb3022683385ea1d40a6f656ba62850002"
     end
   end
 
